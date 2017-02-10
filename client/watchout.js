5b6bbf1751d8f2d5c1bbae09ded8b5922c3cbe32
mrabuse <cogglemat@gmail.com>
2017-02-10 16:30:17 -0600

    Create svg board, add enemies and other styles

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,23 @@
 // start slingin' some d3 here.
+const w = 700;
+const h = 500;
+const svg = d3.select('div.board')
+            .append('svg')
+            .attr('width', w)
+            .attr('height', h);
+
+var _updateEnemies = function () {
+  //Create new enemies
+  var selection = svg.selectAll('image').data([1, 2, 3, 4, 5, 6], id => id)
+                  .enter()
+                  .append('image')
+                  .attr('class', 'enemy')
+                  .attr('xlink:href', 'spaceInvader.gif');
+
+  //Edit existing enemies
+};
+
+//randomize coordinate function;
+
+//
+_updateEnemies();
