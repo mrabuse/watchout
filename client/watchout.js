8e1d72b94db578ac08df9e8676734a217227f958
mrabuse <cogglemat@gmail.com>
2017-02-10 17:20:37 -0600

    Make each enemy start at a unique random location

--- client/watchout.js
+++ client/watchout.js
@@ -6,18 +6,25 @@ const svg = d3.select('div.board')
             .attr('width', w)
             .attr('height', h);
 
-var _updateEnemies = function () {
+const _updateEnemies = function () {
   //Create new enemies
-  var selection = svg.selectAll('image').data([1, 2, 3, 4, 5, 6], id => id)
+  const selection = svg.selectAll('image').data([1, 2, 3, 4, 5, 6], id => id)
                   .enter()
                   .append('image')
                   .attr('class', 'enemy')
+                  .attr('x', () => randomCoor(w - 46))
+                  .attr('y', () => randomCoor(h - 80))
                   .attr('xlink:href', 'spaceInvader.gif');
 
   //Edit existing enemies
+  //.attr('x', randomCoor(w - 46), 'y', randomCoor(h - 80))
 };
 
+//function that accepts element
 //randomize coordinate function;
+const randomCoor = function (max) {
+  return Math.floor(Math.random() * (max + 1));
+};
 
 //
 _updateEnemies();
