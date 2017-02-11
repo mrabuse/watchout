23ae9dff811fa72d0096bcdf0e5fe577c677ffdc
mrabuse <cogglemat@gmail.com>
2017-02-10 19:32:04 -0600

    Create cat hero which follows mouse

--- client/watchout.js
+++ client/watchout.js
@@ -6,6 +6,13 @@ const svg = d3.select('div.board')
             .attr('width', w)
             .attr('height', h);
 
+const cat = svg.selectAll('image')
+               .data(['cat'], id => id)
+               .enter()
+               .append('image')
+               .attr('class', 'cat')
+               .attr('xlink:href', 'balloonCat.gif');
+
 const _updateEnemies = function () {
   //Create new enemies
   const selection = svg.selectAll('image')
@@ -24,7 +31,6 @@ const _updateEnemies = function () {
     .transition().duration(1500).ease('linear')
     .attr('x', () => randomCoor(w - 46))
     .attr('y', () => randomCoor(h - 80));
-  //.attr('x', randomCoor(w - 46), 'y', randomCoor(h - 80))
 
 };
 
@@ -34,5 +40,11 @@ const randomCoor = function (max) {
   return Math.floor(Math.random() * (max + 1));
 };
 
+svg.on('mousemove', function () {
+  var coor = d3.mouse(this);
+  cat.attr('x', coor[0])
+     .attr('y', coor[1]);
+});
 //
+_updateEnemies();
 setInterval(_updateEnemies, 1500);
