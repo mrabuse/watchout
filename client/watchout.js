9112ee6b9daa1dd49d90b7bbc73ce580b2479a94
mrabuse <cogglemat@gmail.com>
2017-02-11 11:05:54 -0600

    Make collisions log

--- client/watchout.js
+++ client/watchout.js
@@ -7,11 +7,12 @@ const svg = d3.select('div.board')
             .attr('height', h);
 
 const cat = svg.selectAll('image')
-               .data(['cat'], id => id)
-               .enter()
-               .append('image')
-               .attr('class', 'cat')
-               .attr('xlink:href', 'balloonCat.gif');
+               .data(['cat'], id => id);
+
+cat.enter()
+   .append('image')
+   .attr('class', 'cat')
+   .attr('xlink:href', 'balloonCat.gif');
 
 const _updateEnemies = function () {
   //Create new enemies
@@ -49,25 +50,40 @@ svg.on('mousemove', function () {
 });
 //
 
+const adjCoor = function(coordinates, xOffset, yOffset) {
+  return [Number(coordinates[0]) + xOffset, Number(coordinates[1]) + yOffset];
+};
+
 const findCatCoor = function () {
   var catCoor = [];
   catCoor.push(cat.attr('x'));
   catCoor.push(cat.attr('y'));
-  return catCoor;
+  return adjCoor(catCoor, 25, 25);
 };
 
-const findEnemyCoor = function () {
+const findEnemyCoor = function (id) {
+  var enemy = svg.selectAll('image').data([id], id => id);
   var enemyCoor = [];
 
-  return enemyCoor;
-};
-const checkPositions = function (catRadius, enemyRadius) {
-  const minDist = (catRadius + enemyRadius);
+  enemyCoor.push(enemy.attr('x'));
+  enemyCoor.push(enemy.attr('y'));
 
-  //var actDist = Math.sqrt(Math.pow((catX - enemyX), 2) + Math.pow((catY - enemyY), 2));
-  //if (actDist <= minDist) {
-  //EXPLODE
-  //}
+  return adjCoor(enemyCoor, 23, 40);
+};
+
+const checkPositions = function () {
+  const minDist = 48; //catRadius(25) + enemyRadius(23)
+  let catCoor = findCatCoor();
+  for (let i = 1; i <= 6; i++) {
+    let enemyCoor = findEnemyCoor(i);
+    let actDist = Math.sqrt(
+      Math.pow((catCoor[0] - enemyCoor[0]), 2) +
+      Math.pow((catCoor[1] - enemyCoor[1]), 2));
+    if (actDist <= minDist) {
+      console.log('Ya got hit!');
+    }
+  }
 };
 _updateEnemies();
+setInterval(checkPositions, 10);
 setInterval(_updateEnemies, 1500);
